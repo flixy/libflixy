e662ed653dd8cb3f4556e8256402d015561868f9
Rylee Fowler <rylee@rylee.me>
2015-08-29 10:00:35 -0500

    move functions out of ctor and add some more

--- index.js
+++ index.js
@@ -5,25 +5,59 @@ var util = require('util');
 // the extensions deal with this?
 function Flixy(opts) {
   var self = this;
-
-  var host = util.format("http%s://%s:%s", (opts.https ? 's' : ''), opts.host, opts.port);
-  this.socket = io(host);
+  this.Opts = opts;
   this.nick = null;
+  this.socket = null;
 
   this.cadmium = null;
-
-  this.SetNetflixCadmium = function(cad) {
-    // test for cadmium methods
-  };
-
-  this.JoinSyncSession = function(sid) {
-    self.socket.emit('flixy join', JSON.encode({ session_id: sid, nick: self.nick }));
-  };
-
-  this.SetNick = function(nick) {
-    self.nick = nick;
-  };
 }
+Flixy.prototype.ConnectToFlixyServer = function(hoststring) {
+  self.socket = io(hoststring);
+  io.on('flixy sync', this.UpdateSession.bind(this));
+  io.on('flixy new session', this.UpdateSession.bind(this));
+  io.on('flixy join session', this.UpdateSession.bind(this));
+  io.on('flixy invalid new init map', function(map){
+    throw {
+      error: "invalid new init map",
+      wrong_map: map,
+    };
+  });
+  io.on('flixy invalid session id', function(invalid_sid){
+    throw {
+      error: "invalid session ID sent",
+      invalid_sid: invalid_sid,
+    };
+  });
+};
+
+Flixy.prototype.SetNetflixCadmium = function(cad) {
+  // test for cadmium methods
+};
+
+Flixy.prototype.JoinSyncSession = function(sid) {
+  if(this.socket) {
+    this.socket.connect();
+    this.socket.emit('flixy join', JSON.encode({ session_id: sid, nick: this.nick }));
+  } else {
+    throw {
+      error: "Tried to use a non-existent socket — you must initialize the socket first!"
+    };
+  }
+};
+
+Flixy.prototype.LeaveSyncSession = function() {
+  if(this.socket) {
+    this.socket.disconnect();
+  } else {
+    throw {
+      error: "socket was null — were you even in a sync session in the first place?"
+    };
+  }
+};
+
+Flixy.prototype.SetNick = function(nick) {
+  this.nick = nick;
+};
 
 Object.defineProperty(Flixy.prototype, "NetflixCadmium", {
   get: function() { return this.cadmium; },
@@ -34,3 +68,5 @@ Object.defineProperty(Flixy.prototype, "Nick", {
   get: function() { return this.nick; },
   set: function(newNick) { this.SetNick(newNick); },
 });
+
+module.exports = Flixy;
